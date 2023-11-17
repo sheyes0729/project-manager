81652abdaa6206dcbdad064b4936f65cf632552c
xiefei <xie0729@yeah.net>
2023-11-17 17:30:38 +0800

    feat: 添加配置

--- src/views/settings/components/ide-settings.vue
+++ src/views/settings/components/ide-settings.vue
@@ -1,9 +1,42 @@
 <script setup lang="ts">
 import SettingItem from './setting-item.vue'
+
+const defaultEditor = ref(null)
+
+const cityList = [
+  {
+    label: '重庆',
+    value: 'cq'
+  },
+  {
+    label: '四川',
+    value: 'sc'
+  },
+  {
+    label: '贵州',
+    value: 'gz'
+  },
+  {
+    label: '云南',
+    value: 'yn'
+  }
+]
 </script>
 
 <template>
   <SettingItem title="IDE配置">
-    <p>添加ide vscode / idea</p>
+    <Form label-position="right" :label-width="120">
+      <Form-item label="编辑器管理">
+        <Button type="primary" size="small">添加编辑器</Button>
+      </Form-item>
+
+      <Form-item label="默认编辑器">
+        <Select v-model="defaultEditor" style="width: 200px" clearable filterable>
+          <Option v-for="item in cityList" :key="item.value" :value="item.value">{{
+            item.label
+          }}</Option>
+        </Select>
+      </Form-item>
+    </Form>
   </SettingItem>
 </template>
--- src/views/settings/components/project-settings.vue
+++ src/views/settings/components/project-settings.vue
@@ -1,12 +1,57 @@
 <script setup lang="ts">
 import SettingItem from './setting-item.vue'
+
+const { system, setSystem } = useStore()
+
+const scanCategory = ref(system.value.scanCategory || 'cover')
+
+const displayMode = ref(system.value.displayMode || 'list')
+
+function scanCategoryChange() {
+  setSystem(scanCategory.value, 'scanCategory')
+}
+
+function displayModeChange() {
+  setSystem(displayMode.value, 'displayMode')
+}
+// const obj = {
+//   name: 'NODE',
+//   icon: '',
+//   include: [],
+//   exclude: [],
+//   ide: ""
+// }
 </script>
 
 <template>
-  <SettingItem title="文件配置">
-    <p>文件绑定</p>
-    <p>扫描策略：覆盖 / 合并</p>
-    <p>分组管理</p>
-    <p>展示管理</p>
+  <SettingItem title="项目配置">
+    <Form :label-width="120" label-position="right">
+      <Form-Item label="类型绑定">
+        <Button type="primary" icon="ios-add" size="small">添加类型</Button>
+      </Form-Item>
+      <Form-Item label="扫描策略">
+        <RadioGroup v-model="scanCategory" @change="scanCategoryChange">
+          <Radio label="cover">
+            <span>覆盖</span>
+          </Radio>
+          <Radio label="merge">
+            <span>合并</span>
+          </Radio>
+        </RadioGroup>
+      </Form-Item>
+      <Form-Item label="分组管理">
+        <Button type="primary" icon="ios-add" size="small">添加分组</Button>
+      </Form-Item>
+      <Form-Item label="展示管理">
+        <RadioGroup v-model="displayMode" @change="displayModeChange">
+          <Radio label="list">
+            <span>列表</span>
+          </Radio>
+          <Radio label="group">
+            <span>分组</span>
+          </Radio>
+        </RadioGroup>
+      </Form-Item>
+    </Form>
   </SettingItem>
 </template>
